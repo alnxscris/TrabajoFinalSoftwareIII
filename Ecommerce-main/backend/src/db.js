305d3ce792f6f418a4173adcee3df9419d9abc54
alnxscris <alaniscris3005@gmail.com>
2025-11-21 00:34:58 -0500

    agregue un puerto, cambie una ruta y quite un path de db.js

--- Ecommerce-main/backend/src/db.js
+++ Ecommerce-main/backend/src/db.js
@@ -1,15 +1,17 @@
 // src/db.js
-import mariadb from 'mariadb';
-import dotenv from 'dotenv';
-dotenv.config();
+import mysql from 'mysql2/promise';
+import * as dotenv from 'dotenv';
+dotenv.config(); // ← ASÍ, SIN PATH
 
-const pool = mariadb.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  connectionLimit: 5
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 pool.getConnection()
